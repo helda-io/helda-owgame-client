34ee3282d7749b8cbcebffa910696ef6c23d63e1
legkvla <vladimir.legkunets@gmail.com>
2019-04-24 22:45:53 +0300

    Working on objects converting

--- src/helda_owgame_client/maps.clj
+++ src/helda_owgame_client/maps.clj
@@ -4,6 +4,27 @@
     )
   )
 
+(defn render-background-layer [entity]
+  (-> entity :attrs :tiles)
+  )
+
+(defn render-objects-layer [entity]
+  (-> entity :attrs :legend)
+  )
+
+(defn convert-room-map-entity [entity]{
+  :room-id (-> entity :attrs :name)
+  :world-id (:world entity)
+  :layers [
+    (render-background-layer entity)
+    (render-objects-layer entity)
+    ]
+  })
+
 (defn load-map [world-id room-id]
-  (client/find-entities world-id ["helda.RoomMap"] [room-id])
+  (some->
+    (client/find-entities world-id ["helda.RoomMap"] [room-id])
+    first
+    convert-room-map-entity
+    )
   )
